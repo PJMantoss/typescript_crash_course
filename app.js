73c5fe3d89334c2763dc2e9fd2b01a4f88d74615
PJMantoss <joelptoss@gmail.com>
2021-05-29 17:22:50 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("hi");
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.greet = function () {
+        return "Hi! my name is " + this.name + " and I am " + this.age;
+    };
+    ;
+    return Person;
+}());
+var mantoss = new Person("Mantoss", 35);
+console.log(mantoss.greet());
